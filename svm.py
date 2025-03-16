523fed4436386b64a798d94a845031da369487ed
Ezel00 <74823813+Ezel00@users.noreply.github.com>
2025-03-16 13:24:10 +0200

    Update svm.py

--- svm.py
+++ svm.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
+import processStats as ps
 
 
 pathEmbedTest=r"C:\Users\ezele\Desktop\thesis\tdaPython\final\test.csv"
