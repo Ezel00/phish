74e75665d2ca571cebbca37e6e0a795d661da6f6
Ezel00 <74823813+Ezel00@users.noreply.github.com>
2025-03-16 12:05:44 +0200

    Update logReg.py

--- logReg.py
+++ logReg.py
@@ -24,16 +24,23 @@ XTest =test.drop(columns="label")
 yTest = test["label"]
 
 def dirtyY(yT, flip, random_seed=42):
-    old = yT.copy()
     np.random.seed(random_seed)
     yT = np.array(yT)
     zeros = np.where(yT == 0)[0]
-    num = int(len(zeros) * flip)
-    inds = np.random.choice(zeros, num, replace=False)
-    yT[inds] = 1
-    print("Original Label Distribution:\n", np.bincount(old))
-    print("Dirty Label Distribution:\n", np.bincount(yT))
+    ones = np.where(yT == 1)[0]
+    num_zeros = int(len(zeros) * flip)
+    num_ones = int(len(ones) * flip)
+    zero_inds = np.random.choice(zeros, num_zeros, replace=False)
+    one_inds = np.random.choice(ones, num_ones, replace=False)
+    yT[zero_inds] = 1  # Flip 0 -> 1
+    #yT[one_inds] = 0  # Flip 1 -> 0
+    print(f"Original Label Distribution: 0s = {len(zeros)}, 1s = {len(ones)}")
+    new_zeros = np.sum(yT == 0)
+    new_ones = np.sum(yT == 1)
+    print(f"Dirty Label Distribution: 0s = {new_zeros}, 1s = {new_ones}")
     return yT
+
+
 
 
 def logReg(X,y):
